609a6725944decced16903b21aa7db7563412e57
auscarter17 <auscarter17@gmail.com>
2023-03-31 19:04:23 -0400

    math functions are working

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.btn');
 const numButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
-const equalsButton = document.querySelectorAll('.equals');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
 
 display.innerHTML = 0;
 
@@ -10,25 +11,49 @@ let firstNum
 let operator
 let secondNum
 
-function add(num1, num2) {
-  return (num1 + num2);
+function add(firstNum, secondNum) {
+  // variables will concatenate without parseFloat in place
+  return ((parseFloat(firstNum) + parseFloat(secondNum)).toFixed(2));
 };
 
-function subtract(num1, num2) {
-  return (num1 - num2);
+function subtract(firstNum, secondNum) {
+  return ((firstNum - secondNum).toFixed(2));
 };
 
-function multiply(num1, num2) {
-  return (num1 * num2);
+function multiply(firstNum, secondNum) {
+  return ((firstNum * secondNum).toFixed(2));
 }
 
-function divide(num1, num2) {
-  return (num1 / num2);
+function divide(firstNum, secondNum) {
+  return ((firstNum / secondNum).toFixed(2));
 };
+
+function clear() {
+  firstNum = null
+  operator = null
+  secondNum = null
+}
+
+clearButton.addEventListener('click', function() {
+  clear();
+  display.innerHTML = 0;
+});
+
+equalsButton.addEventListener('click', calculate);
 
 function calculate() {
-  
-}
+  if (!firstNum || !secondNum || !operator) {
+    display.innerHTML = "Error";
+  } else if (operator === 'times') {
+    display.innerHTML = multiply(firstNum, secondNum)
+  } else if (operator === 'plus') {
+    display.innerHTML = add(firstNum, secondNum)
+  } else if (operator === 'minus') {
+    display.innerHTML = subtract(firstNum, secondNum)
+  } else if (operator === 'divide') {
+    display.innerHTML = divide(firstNum, secondNum)
+  }
+};
 
 numButtons.forEach(numButton => {
   numButton.addEventListener('click', function() {
@@ -40,7 +65,7 @@ numButtons.forEach(numButton => {
         firstNum = firstNum + numButton.id;
         display.innerHTML = firstNum;
       }
-    } else {
+    } else if (operator) {
       if (!secondNum) {
         secondNum = numButton.id;
         display.innerHTML = numButton.id;
@@ -54,6 +79,17 @@ numButtons.forEach(numButton => {
 
 operatorButtons.forEach(operatorButton => {
   operatorButton.addEventListener('click', function() {
-    operator = operatorButton.id;
+    if (!firstNum) {
+      display.innerHTML = "Error"
+      clear();
+    } else if (firstNum && secondNum && operator) {
+      calculate();
+      firstNum = display.innerHTML;
+      secondNum = null;
+      operator = operatorButton.id;
+    } else {
+      operator = operatorButton.id;
+      console.log(operator);
+    }
   });
 });
