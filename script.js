fa2b0a965d4035f5f9b4fe4a0dda4c7539c2ff5d
auscarter17 <auscarter17@gmail.com>
2023-03-31 18:10:23 -0400

    added button functionality for displaying numbers

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.btn');
+const numButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelectorAll('.equals');
 
 display.innerHTML = 0;
 
@@ -21,3 +25,35 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return (num1 / num2);
 };
+
+function calculate() {
+  
+}
+
+numButtons.forEach(numButton => {
+  numButton.addEventListener('click', function() {
+    if (!operator) {
+      if (!firstNum) {
+        firstNum = numButton.id;
+        display.innerHTML = numButton.id;
+      } else {
+        firstNum = firstNum + numButton.id;
+        display.innerHTML = firstNum;
+      }
+    } else {
+      if (!secondNum) {
+        secondNum = numButton.id;
+        display.innerHTML = numButton.id;
+      } else {
+        secondNum = secondNum + numButton.id;
+        display.innerHTML = secondNum;
+      }
+    }
+  });
+});
+
+operatorButtons.forEach(operatorButton => {
+  operatorButton.addEventListener('click', function() {
+    operator = operatorButton.id;
+  });
+});
