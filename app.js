07949720612fd458d944171c65285f12842aa5c4
auscarter17 <auscarter17@gmail.com>
2025-02-25 12:40:55 -0500

    display functioning properly with typed keys, entering a number after the operator resets the display the show the second number

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ let numberKeys = document.querySelectorAll('.number')
 let operatorKeys = document.querySelectorAll('.operator')
 
 // create variables to store numbers and operators from key presses
-let firstValue = 10
-let secondValue = 10
-let operator
+let firstValue
+let secondValue
+let operatorValue
 
 // create variable for values to be stored in display
-let currentValue = 100
+let currentValue = 0
 
 display.textContent = currentValue
 
@@ -21,13 +21,36 @@ for (let btn of btns) {
     btnType = e.target.className
 
     // if clear button is selected, always reset display and clear all values
-    if (btnType = clear) {
-      console.log(btnType)
+    if (btnType === "clear") {
       firstValue = undefined
       secondValue = undefined
       operator = undefined
       currentValue = 0
       display.textContent = currentValue
+
+    // if key press is operator key, check for other numbers present  
+    } else if (btnType === 'operator') {
+      if (firstValue) {
+        operatorValue = e.target.id
+        console.log(operatorValue)
+      }
+    } else if (btnType === 'number') {
+      // if no other numbers have been pressed, assign number to first value
+      if (!firstValue) {
+        firstValue = e.target.id
+        display.textContent = firstValue
+      } else if (firstValue) {
+        if (!operatorValue) {
+          firstValue = (firstValue + e.target.id)
+          display.textContent = firstValue
+        } else if (operatorValue && !secondValue) {
+          secondValue = e.target.id
+          display.textContent = secondValue
+        } else if (operatorValue && secondValue) {
+          secondValue = (secondValue + e.target.id)
+          display.textContent = secondValue
+        }
+      }
     }
     
   })
